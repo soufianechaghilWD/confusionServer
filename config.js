7555b6dd0d3daa5f124f2234efcb16a7ea4ea3a6
soufiane chaghil <soufianechaghil1998@gmail.com>
2020-10-05 16:43:57 +0000

    Using OAuth with Passport and Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "351016339434609",
+    clientSecret: "355a3d7dd9a8c89b3fa2175e766c36fe",
+  },
 };
